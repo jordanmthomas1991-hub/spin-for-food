0cbd57a7be891e2093d70b1b55374c09f70da8ea
jordanmthomas1991-hub <jordanmthomas1991@gmail.com>
2026-05-19 09:42:37 +0100

    open/close

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // SpinForFood Service Worker
 // Caches the site for offline use
 
-const CACHE_NAME = 'spinforfood-v4';
+const CACHE_NAME = 'spinforfood-v5';
 const ASSETS_TO_CACHE = [
   '/',
   '/index.html',
